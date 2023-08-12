ceaf6acaf3f047ea573ef35310340a8c6bcfb084
Tanzim <tanzim008@gmail.com>
2023-08-12 00:34:58 -0230

    list component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
+const workHistory = [
+  {
+    id: 1,
+    title: 'CSR',
+    description: 'Customer Service rep in the claims department. Started in November 2022.',
+    role: ['create a claim','assist inusred', 'adjust claim'],
+  },
+  {
+    id: 2,
+    title: 'Cashier',
+    description: 'Front-end cashier in Sobeys. Started in September 2022.',
+    role: ['Serve customer','provide product knowledge', 'Front end clerk'],
+  },
+  {
+    id: 3,
+    title: 'Web dev',
+    description: 'Web developer in HYPE Dhaka. Started in June 2018 till June 2022.',
+    role: ['Develope website','Revamp website', 'Designining wireframe'],
+  },
+];
 
 function App() {
+  const listItems = workHistory.map((work)=>{
+    const listItem = `${work.title} - ${work.description}`;
+    return <li>{listItem}</li>
+  });
   return (
-    <div className="App">
+    <div>
      <h1>Rendering list in React</h1>
+     <h2>Work History</h2>
+     <ul>
+      {listItems}
+     </ul>
     </div>
   );
 }
